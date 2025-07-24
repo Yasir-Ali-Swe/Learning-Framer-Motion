5aa632189c0e14e206412c63d1f695b38f4a5851
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-24 22:20:41 +0500

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl underline bg-red-600 text-white text-center py-4'>Learning Framer Motion</h1>
+    </div>
+  )
+}
+
+export default App
