e5a334c59082bbe0fa22bc67cc56de116de3cbd4
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-25 17:24:42 +0500

    Entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import TextAnimation from './component/TextAnimation'
 import TypeWriteAnimation from "./component/TypeWriteAnimation"
+import ScrollAnimation from "./component/ScrollAnimation"
 
 const App = () => {
   return (
     <>
-    <TypeWriteAnimation />
+    <ScrollAnimation />
     </>
   )
 }
