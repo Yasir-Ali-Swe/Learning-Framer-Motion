0957c8e6526dcbc4d011460d3c5f5b897af612e2
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-07-25 17:24:27 +0500

    ScrollAnimation with progress bar

--- src/component/ScrollAnimation.jsx
+++ src/component/ScrollAnimation.jsx
@@ -42,37 +42,46 @@ const ScrollAnimation = () => {
     })
     const x = useTransform(scrollYProgress, [0, 1], ["0", "-60%"])
     return (
-        <div className="w-full h-full bg-stone-500">
-            {/* Upper Div */}
-            <div className="w-full h-[250px] flex justify-center items-center">
-                <h1 className="text-4xl font-bold text-stone-200">Upper Die</h1>
-            </div>
-            {/* Animation Div */}
-            <div ref={targetRef} className="relative bg-stone-600 w-full h-[200vh] ">
-                <div className="w-full sticky top-0 left-0 h-screen flex items-center overflow-hidden">
-                <motion.div  style={{x}} className="flex gap-4 px-4">
-                    {
-                        images.map((item)=>{
-                            return(
-                                <div key={item.tittle} className="w-[280px] h-[280px] relative overflow-hidden">
-                                    <div>
-                                        <img src={item.img} className="rounded-xl w-full absolute transition-transform duration-300"/>
-                                    </div>
-                                    <div className="absolute inset-0 grid place-content-center z-10">
-                                        <h1 className="text-3xl text-stone-300 backdrop-blur-sm px-4 py-2">{item.tittle}</h1>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
-                </motion.div>
+        <motion.div className="w-full h-full relative">
+            <motion.div 
+                className="fixed top-0 left-0 w-full h-1 bg-red-800 z-50"
+                style={{
+                    scaleX: scrollYProgress,
+                    transformOrigin: 'left'
+                }}
+            />
+            <div className="w-full h-full bg-stone-500">
+                {/* Upper Div */}
+                <div className="w-full h-[250px] flex justify-center items-center">
+                    <h1 className="text-4xl font-bold text-stone-200">Upper Die</h1>
+                </div>
+                {/* Animation Div */}
+                <div ref={targetRef} className="relative bg-stone-600 w-full h-[200vh] ">
+                    <div className="w-full sticky top-0 left-0 h-screen flex items-center overflow-hidden">
+                        <motion.div style={{ x }} className="flex gap-4 px-4">
+                            {
+                                images.map((item) => {
+                                    return (
+                                        <div key={item.tittle} className="w-[280px] h-[280px] relative overflow-hidden">
+                                            <div>
+                                                <img src={item.img} className="rounded-xl w-full absolute transition-transform duration-300" />
+                                            </div>
+                                            <div className="absolute inset-0 grid place-content-center z-10">
+                                                <h1 className="text-3xl text-stone-300 backdrop-blur-sm px-4 py-2">{item.tittle}</h1>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </motion.div>
+                    </div>
+                </div>
+                {/* Lower Div */}
+                <div className="w-full h-[250px] flex justify-center items-center">
+                    <h1 className="text-4xl font-bold text-stone-200">Lower Die</h1>
                 </div>
             </div>
-            {/* Lower Div */}
-            <div className="w-full h-[250px] flex justify-center items-center">
-                <h1 className="text-4xl font-bold text-stone-200">Lower Die</h1>
-            </div>
-        </div>
+        </motion.div>
     )
 }
 export default ScrollAnimation;
